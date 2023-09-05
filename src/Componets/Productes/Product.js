2777d6c2f408664df2873050d961744afd7d8c96
Masoumeh Rezai <you@example.com>
2023-09-06 03:01:58 +0430

    add new route

--- src/Componets/Productes/Product.js
+++ src/Componets/Productes/Product.js
@@ -1,10 +1,16 @@
 import { Fragment } from "react";
-import classes from './Product.module.css'
+import classes from './Product.module.css';
+import {Route}  from 'react-router-dom'
+
 
 const Product = (props) => {
     return (
         <Fragment>
             <div className={classes.product}>
+            <Route>
+                
+            </Route>
+
             </div>
         </Fragment>
     )
---
+++ src/Componets/Productes/ProduectOne.js
@@ -0,0 +1,10 @@
+import { Fragment } from "react";
+
+const ProductOne=(props)=>{
+    return(
+        <Fragment>
+
+        </Fragment>
+    )
+}
+export default ProductOne
