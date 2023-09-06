f32edfe10a3009c587c07878ed0b7d943ee947cb
Masoumeh Rezai <you@example.com>
2023-09-06 23:06:46 +0430

    add work on list music

---
+++ src/Componets/UIMusic/ErrorModal.js
@@ -0,0 +1,38 @@
+
+import ReactDom from 'react-dom';
+import Card from './Card';
+import Button from './Button';
+import classes from './ErrorModal.module.css';
+import { Fragment } from 'react';
+const Backdrop = (props) => {
+    return <div className={classes.backdrop} onClick={props.onconfirm} />
+}
+const Modaloerlay = (props) => {
+    return (
+        <Card className={classes.modal}>
+            <form>
+                <h2>
+                    Add Your Information
+                </h2>
+                <input placeholder='Name' />
+                <input placeholder='Email' />
+                <input placeholder='Phone Number' />
+                <input placeholder='Adress' />
+                <button>send</button>
+
+
+            </form>
+        </Card>
+    )
+}
+const ErroModal = (props) => {
+    return (
+        <Fragment>
+            {ReactDom.createPortal(<Backdrop onconfirm={props.onconfirm} />, document.getElementById('backdrop-root'))}
+            {ReactDom.createPortal(<Modaloerlay title={props.title} massage={props.massage} onconfirm={props.onconfirm} />,
+                document.getElementById('overlay-root'))}
+        </Fragment>
+    )
+}
+
+export default ErroModal;
